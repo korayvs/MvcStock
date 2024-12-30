f35bf3bc1b816ffc937aabf6b0f568f976293155
Koray Vatansever <koray-vatansever@hotmail.com>
2024-12-30 20:52:40 +0300

    update

--- MvcStok/Controllers/MusteriController.cs
+++ MvcStok/Controllers/MusteriController.cs
@@ -9,7 +9,7 @@ namespace MvcStok.Controllers
 {
     public class MusteriController : Controller
     {
-        // GET: Musteri
+        
         MvcDbStokEntities db = new MvcDbStokEntities();
         public ActionResult Index()
         {
@@ -24,9 +24,33 @@ namespace MvcStok.Controllers
         [HttpPost]
         public ActionResult YeniMusteri(TBLMUSTERILER p1)
         {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniMusteri");
+            }
             db.TBLMUSTERILER.Add(p1);
             db.SaveChanges();
             return View();
+        }
+        public ActionResult SIL(int id)
+        {
+            var musteri = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", mus);
+        }
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var musteri = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            musteri.MUSTERIAD = p1.MUSTERIAD;
+            musteri.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- MvcStok/Controllers/UrunController.cs
+++ MvcStok/Controllers/UrunController.cs
@@ -19,14 +19,42 @@ namespace MvcStok.Controllers
         [HttpGet]
         public ActionResult UrunEkle()
         {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = i.KATEGORIAD,
+                                                Value = i.KATEGORIID.ToString()
+                                            }).ToList();
+            ViewBag.dgr = degerler;
             return View();
         }
         [HttpPost]
         public ActionResult UrunEkle(TBLURUNLER p1)
         {
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            p1.TBLKATEGORILER = ktg;
             db.TBLURUNLER.Add(p1);
             db.SaveChanges();
-            return View();
+            return RedirectToAction("Index");
+        }
+        public ActionResult SIL(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View("UrunGetir", urun);
         }
     }
 }
